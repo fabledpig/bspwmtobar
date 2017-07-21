ef6e106e097f1dd5d318d017b4fea255aa2b2dd9
Lénárt Levente <fabledpig@gmail.com>
2017-07-21 17:52:52 +0200

    Test some function.

--- bspwmtobar.c
+++ bspwmtobar.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#include "desktop.h"
+#include "tile.h"
 
 #define BUF_SIZE 512
 
@@ -13,13 +14,21 @@ int main(int argc, char *argv[])
 	mkfifo(fifo_path, 0666);
 	int fd = open(fifo_path, O_RDONLY);
 
+	tile *tile_array = malloc(sizeof(tile)); //this part is hardcoded, just for testing
+	unsigned int size = 1;
+	tile_array[0] = create_tile("W", 'c');
+	sort_tile_array(tile_array, size);
+
 	char buf[BUF_SIZE];
 	int bytes_read;
 	while((bytes_read = read(fd, buf, BUF_SIZE - 1)) > 0) {
 		buf[bytes_read] = 0;
-		printf("%s", buf);
+		update_tile_array(tile_array, size, buf);
+
+		printf("%s\n", tile_array[0].str);
 	}
 
+	free_tile_array(tile_array, size);
 	close(fd);
 	unlink(fifo_path);
 	return 0;
