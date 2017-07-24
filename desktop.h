f1e4993c5030ea5b243bc68526777f1d1c6e00b1
Lénárt Levente <fabledpig@gmail.com>
2017-07-24 11:09:34 +0200

    Move color variables declaration.

--- desktop.h
+++ desktop.h
@@ -8,10 +8,6 @@ typedef struct {
 	char status;
 } desktop;
 
-extern char occupied_color[COLOR_LENGTH], occupied_a_color[COLOR_LENGTH],
-			free_color[COLOR_LENGTH], free_a_color[COLOR_LENGTH],
-			urgent_color[COLOR_LENGTH], urgent_a_color[COLOR_LENGTH];
-
 unsigned int count_desktops(const char *bspwm_output);
 
 void build_desktop_array(const char *bspwm_output, desktop **desktop_array, unsigned int *size);
